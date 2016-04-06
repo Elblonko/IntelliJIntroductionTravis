2e70da515c99f182a3ccb1093bbd5b48fee728ee
elblonko <elblonko@gmail.com>
2016-04-05 17:12:11 -0700

    testing travis

---
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -0,0 +1,29 @@
+package edu.ucsd.cs110.temperature;
+
+/**
+ * Created by cs110xmatt on 3/11/16.
+ */
+public class Celsius extends Temperature {
+
+        public Celsius(float t)
+        {
+            super(t);
+        }
+
+
+        public String toString()
+        {
+            // TODO: Complete this method
+            return "";
+        }
+
+    @Override
+    public Temperature toCelsius() {
+        return null;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return null;
+    }
+}
